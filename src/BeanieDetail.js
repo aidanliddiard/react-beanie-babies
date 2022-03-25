3d18d9bc5e81141a8c53a0128b5c2172b84ce1e1
aidanliddiard <aidanliddiard@gmail.com>
2022-03-25 15:56:23 -0700

    forgot to commit beanies showing up on list page

--- src/BeanieDetail.js
+++ src/BeanieDetail.js
@@ -1,15 +1,21 @@
 import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { getSingleBeanie } from './services/fetch-utils';
 
 export default function BeanieDetail() {
   // you'll need to track the current beanieBaby in state
   // you'll need to get the route param using the appropriate react router hook
+  const params = useParams();
 
   useEffect(() => {
     // you'll need to define a fetch function here (then call it below) that gets this page's beanie baby and injects it into state using the correct state handler
-
-  }, []); // note that you'll want the id from the url in the dependency array because you want the useEffect callback to get called every time the url changes 
+    const fetchData = async () => {
+      const data = await getSingleBeanie();
+      console.log(data);
+    };
+    fetchData();
+  }, []); // note that you'll want the id from the url in the dependency array because you want the useEffect callback to get called every time the url changes
 
   function handleBeanieClick() {
     // here's a challenge. How can you link on click to the beanie baby's correct entry in the official beanie baby fan site?
@@ -18,7 +24,7 @@ export default function BeanieDetail() {
   return (
     <>
       {/* Feel free to uncomment and use the below code--but you'll need to figure out how to keep it from blowing up on load */}
-      
+
       {/* <Link to='/'>Home</Link>
       <div className='beanie-detail' onClick={handleBeanieClick}>
         <div className='beanie-data'>
--- src/BeaniesList.js
+++ src/BeaniesList.js
@@ -1,8 +1,17 @@
-import BeanieBaby from './BeanieBaby.js';
+import { Link } from 'react-router-dom';
 
 export default function BeaniesList({ beanieBabies }) {
   return (
-    <div className='beanies'>
+    <div className="beanies">
       {/* render out the beanie babies as a list */}
-    </div>);
+      {beanieBabies.map((baby) => (
+        <div key={baby.id} style={{ margin: `10px` }}>
+          <img src={baby.image} style={{ width: `200px` }} />
+          <Link path to={`/beanies/${baby.id}`}>
+            <h4>{baby.title}</h4>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 }
